a6b998558e10b2e8fa1a80516db9d77db1c88ad6
Brendan <brendantsuda@gmail.com>
2023-01-08 18:43:35 -0800

    store origin data in store

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,8 +4,12 @@ import tw from 'tailwind-react-native-classnames';
 import NavOptions from '../components/NavOptions';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from 'react-redux';
+import { setDestination, setOrigin } from '../slices/navSlice';
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+
   return (
     <SafeAreaView style={tw `bg-white h-full`}>
       <View style={tw `p-5`}>
@@ -17,8 +21,6 @@ const HomeScreen = () => {
                 uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Uber_logo_2018.svg/2560px-Uber_logo_2018.svg.png"
             }}
         />
-        {/* <View style={tw `flex-1`}> */}
-
         <GooglePlacesAutocomplete 
           styles={{
             container: {
@@ -32,15 +34,25 @@ const HomeScreen = () => {
             }
           }}
           enablePoweredByContainer={false}
+          onPress={(data, details = null) => {
+            dispatch(setOrigin({
+              location: details.geometry.location,
+              description: data.description
+            }));
+
+            dispatch(setDestination(null));
+          }}
           query={{
             key: GOOGLE_MAPS_APIKEY,
             language: 'en'
           }}
+          returnKeyType={'search'}
+          fetchDetails={true}
+          minLength={2}
           placeholder='Where From?'
           nearbyPlacesAPI='GooglePlacesSearch'
           debounce={400}
         />
-        {/* </View> */}
         <NavOptions />
       </View>
     </SafeAreaView>
