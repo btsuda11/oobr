c20a0585788f86d1579d72e8ba9ac6679d524267
Brendan <brendantsuda@gmail.com>
2023-01-08 18:20:23 -0800

    enable google places autocomplete

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
 import NavOptions from '../components/NavOptions';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from "@env";
 
 const HomeScreen = () => {
   return (
@@ -15,6 +17,30 @@ const HomeScreen = () => {
                 uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Uber_logo_2018.svg/2560px-Uber_logo_2018.svg.png"
             }}
         />
+        {/* <View style={tw `flex-1`}> */}
+
+        <GooglePlacesAutocomplete 
+          styles={{
+            container: {
+              flex: 0
+            },
+            textInput: {
+              fontSize: 18
+            },
+            listView: {
+              zIndex: 10
+            }
+          }}
+          enablePoweredByContainer={false}
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: 'en'
+          }}
+          placeholder='Where From?'
+          nearbyPlacesAPI='GooglePlacesSearch'
+          debounce={400}
+        />
+        {/* </View> */}
         <NavOptions />
       </View>
     </SafeAreaView>
