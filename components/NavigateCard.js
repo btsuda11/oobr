c5982a6f9637c3c11f76f6b0024debbbabfeb717
Brendan <brendantsuda@gmail.com>
2023-01-08 19:14:43 -0800

    create navigate card

---
+++ components/NavigateCard.js
@@ -0,0 +1,63 @@
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import React from 'react';
+import tw from 'tailwind-react-native-classnames';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from '@env';
+import { setDestination } from '../slices/navSlice';
+import { useDispatch } from 'react-redux';
+import { useNavigation } from '@react-navigation/native';
+
+const NavigateCard = () => {
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
+
+  return (
+    <SafeAreaView style={tw `flex-1 bg-white`}>
+      <Text style={tw `text-center py-5 text-xl`}>Good Morning, Demo User</Text>
+      <View style={tw `border-t border-gray-200 flex-shrink`}>
+        <View>
+            <GooglePlacesAutocomplete 
+                placeholder='Where To?'
+                styles={toInputBoxStyles}
+                returnKeyType={'search'}
+                onPress={(data, details = null) => {
+                    dispatch(setDestination({
+                        location: details.geometry.location,
+                        description: data.description
+                    }));
+
+                    navigation.navigate('RideOptionsCard');
+                }}
+                nearbyPlacesAPI='GooglePlacesSearch'
+                fetchDetails={true}
+                enablePoweredByContainer={false}
+                query={{
+                    key: GOOGLE_MAPS_APIKEY,
+                    language: 'en'
+                }}
+                debounce={400}
+            />
+        </View>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default NavigateCard
+
+const toInputBoxStyles = StyleSheet.create({
+    container: {
+        backgroundColor: 'white',
+        paddingTop: 20,
+        flex: 0
+    },
+    textInput: {
+        backgroundColor: '#DDDDDF',
+        borderRadius: 0,
+        fontSize: 18
+    },
+    textInputContainer: {
+        paddingHorizontal: 20,
+        paddingBottom: 0
+    }
+})
