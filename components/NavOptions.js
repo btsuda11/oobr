eddac8cc1f3fbc9826316e75e15b77e0834c484c
Brendan <brendantsuda@gmail.com>
2023-01-08 17:33:40 -0800

    set up google maps api

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,3 +1,4 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { View, Text, Image, FlatList, TouchableOpacity } from 'react-native';
 import { Icon } from 'react-native-elements';
@@ -19,6 +20,8 @@ const data = [
 ];
 
 const NavOptions = () => {
+  const navigation = useNavigation();
+
   return (
     <FlatList 
         data={data}
@@ -27,6 +30,7 @@ const NavOptions = () => {
         renderItem={({ item }) => (
             <TouchableOpacity
                 style={tw `p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+                onPress={() => navigation.navigate(item.screen)}
             >
                 <View>
                     <Image 
