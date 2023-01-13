159283eeabe1ca2fb789d5d780b44a2053f175ae
Brendan <brendantsuda@gmail.com>
2023-01-13 11:01:21 -0800

    fetch travel time info

--- components/Map.js
+++ components/Map.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { selectDestination, selectOrigin } from '../slices/navSlice';
+import { useDispatch } from 'react-redux';
+import { selectDestination, selectOrigin, setTravelTimeInformation } from '../slices/navSlice';
 import MapView, { Marker } from 'react-native-maps';
 import tw from 'tailwind-react-native-classnames';
 import { useSelector } from 'react-redux';
@@ -11,6 +12,7 @@ const Map = () => {
   const origin = useSelector(selectOrigin);
   const destination = useSelector(selectDestination);
   const mapRef = useRef(null);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     if (!origin || !destination) return;
@@ -20,6 +22,18 @@ const Map = () => {
         edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
     });
   }, [origin, destination])
+
+  useEffect(() => {
+    if (!origin || !destination) return;
+
+    const getTravelTime = async () => {
+        fetch(`https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${origin.description}&destinations=${destination.description}&key=${GOOGLE_MAPS_APIKEY}`).then(res => res.json()).then(data => {
+            dispatch(setTravelTimeInformation(data.rows[0].elements[0]))
+        });
+    }
+
+    getTravelTime();
+  }, [origin, destination, GOOGLE_MAPS_APIKEY])
 
   return (
     <MapView
--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,8 +1,10 @@
 import { useNavigation } from '@react-navigation/native';
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import { StyleSheet, Text, View, SafeAreaView, Image, TouchableOpacity, FlatList } from 'react-native';
 import { Icon } from 'react-native-elements';
 import tw from 'tailwind-react-native-classnames';
+import { selectTravelTimeInformation } from '../slices/navSlice';
 
 const data = [
   {
@@ -27,6 +29,8 @@ const data = [
 
 const RideOptionsCard = () => {
   const navigation = useNavigation();
+  const [selected, setSelected] = useState(null);
+  const travelTimeInformation = useSelector(selectTravelTimeInformation);
 
   return (
     <SafeAreaView style={tw `bg-white flex-grow`}>
@@ -34,14 +38,14 @@ const RideOptionsCard = () => {
         <TouchableOpacity style={tw `absolute top-3 left-5 z-50 p-3 rounded-full`} onPress={() => navigation.navigate('NavigateCard')}>
           <Icon name='chevron-left' type='fontawesome' />
         </TouchableOpacity>
-        <Text style={tw `text-center py-5 text-xl`}>Select a Ride</Text>
+        <Text style={tw `text-center py-5 text-xl`}>Select a Ride - {travelTimeInformation?.distance.text}</Text>
       </View>
 
       <FlatList 
         data={data}
         key={item => item.id}
         renderItem={({ item: { id, title, multiplier, image }, item }) => (
-          <TouchableOpacity style={tw `flex-row justify-between items-center px-10`}>
+          <TouchableOpacity style={tw `flex-row justify-between items-center px-10 ${id === selected?.id && 'bg-gray-200'}`} onPress={() => setSelected(item)}>
             <Image 
               style={{
                 width: 100,
@@ -52,12 +56,17 @@ const RideOptionsCard = () => {
             />
             <View style={tw `-ml-6`}>
               <Text style={tw `text-xl font-semibold`}>{title}</Text>
-              <Text>Travel time...</Text>
+              <Text>{travelTimeInformation?.duration.text} Travel Time</Text>
             </View>
             <Text style={tw `text-xl`}>$99</Text>
           </TouchableOpacity>
         )}
       />
+      <View>
+        <TouchableOpacity disabled={!selected} style={tw `bg-black py-3 m-3 ${!selected && 'bg-gray-300'}`}>
+          <Text style={tw `text-center text-white text-xl`}>Choose {selected?.title}</Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   )
 }
