0aac265c2f834566f20e29bde5959fa5d5f1518a
Brendan <brendantsuda@gmail.com>
2023-01-13 10:42:11 -0800

    render ride options

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,11 +1,64 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { useNavigation } from '@react-navigation/native';
+import React from 'react';
+import { StyleSheet, Text, View, SafeAreaView, Image, TouchableOpacity, FlatList } from 'react-native';
+import { Icon } from 'react-native-elements';
+import tw from 'tailwind-react-native-classnames';
+
+const data = [
+  {
+    id: 'Uber-X-123',
+    title: 'UberX',
+    multiplier: 1,
+    image: 'https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,w_485,h_385/f_auto,q_auto/products/carousel/UberX.png'
+  },
+  {
+    id: 'Uber-XL-456',
+    title: 'UberXL',
+    multiplier: 1.2,
+    image: 'https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,w_485,h_385/f_auto,q_auto/products/carousel/UberXL.png'
+  },
+  {
+    id: 'Uber-LUX-789',
+    title: 'UberLUX',
+    multiplier: 1.75,
+    image: 'https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,w_485,h_385/f_auto,q_auto/products/carousel/Lux.png'
+  }
+]
 
 const RideOptionsCard = () => {
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>RideOptionsCard</Text>
-    </View>
+    <SafeAreaView style={tw `bg-white flex-grow`}>
+      <View>
+        <TouchableOpacity style={tw `absolute top-3 left-5 z-50 p-3 rounded-full`} onPress={() => navigation.navigate('NavigateCard')}>
+          <Icon name='chevron-left' type='fontawesome' />
+        </TouchableOpacity>
+        <Text style={tw `text-center py-5 text-xl`}>Select a Ride</Text>
+      </View>
+
+      <FlatList 
+        data={data}
+        key={item => item.id}
+        renderItem={({ item: { id, title, multiplier, image }, item }) => (
+          <TouchableOpacity style={tw `flex-row justify-between items-center px-10`}>
+            <Image 
+              style={{
+                width: 100,
+                height: 100,
+                resizeMode: 'contain'
+              }}
+              source={{ uri: image }}
+            />
+            <View style={tw `-ml-6`}>
+              <Text style={tw `text-xl font-semibold`}>{title}</Text>
+              <Text>Travel time...</Text>
+            </View>
+            <Text style={tw `text-xl`}>$99</Text>
+          </TouchableOpacity>
+        )}
+      />
+    </SafeAreaView>
   )
 }
 
